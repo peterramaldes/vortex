734c162f894d3adc9ea59d988e230a8475d3c0b1
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2023-11-13 14:45:09 -0300

    Replying the Echo and EchoOk

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::io::StdoutLock;
-
 use serde::{Deserialize, Serialize};
+use std::{io::StdoutLock, u64};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-struct Message {
+pub struct Message {
     /// Identification of the node that this message come from
     src: String,
 
@@ -15,10 +14,12 @@ struct Message {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-struct Body {
+pub struct Body {
     /// The identification of the message
     #[serde(rename = "msg_id")]
-    id: u64,
+    id: Option<usize>,
+
+    in_reply_to: Option<usize>,
 
     #[serde(flatten)]
     payload: Payload,
@@ -27,20 +28,56 @@ struct Body {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
-enum Payload {
-    Echo {
-        /// The message that will be apper on STDOUT
-        echo: String,
-    },
+pub enum Payload {
+    Echo { echo: String },
+    EchoOk { echo: String },
+}
+
+pub struct Node {
+    id: usize,
+}
+
+impl Node {
+    pub fn reply(
+        &mut self,
+        input: Message,
+        stdout: &mut serde_json::Serializer<StdoutLock>,
+    ) -> anyhow::Result<()> {
+        match input.body.payload {
+            Payload::Echo { echo } => {
+                let reply = Message {
+                    src: input.dest,
+                    dest: input.src,
+                    body: Body {
+                        id: Some(self.id),
+                        in_reply_to: input.body.id,
+                        payload: Payload::EchoOk { echo },
+                    },
+                };
+
+                reply.serialize(stdout)?;
+
+                self.id += 1;
+            }
+            Payload::EchoOk { echo: _ } => todo!(),
+        }
+
+        Ok(())
+    }
 }
 
 fn main() -> anyhow::Result<()> {
     let stdin = std::io::stdin().lock();
-
     let inputs = serde_json::Deserializer::from_reader(stdin).into_iter::<Message>();
+
+    let stdout = std::io::stdout().lock();
+    let mut output = serde_json::Serializer::new(stdout);
+
+    let mut state = Node { id: 0 };
+
     for input in inputs {
         let input = input?;
-        println!("{:?}", input);
+        state.reply(input, &mut output)?
     }
 
     Ok(())
